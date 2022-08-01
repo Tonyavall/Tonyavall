a9504338d2fd59cc102510b7e182725c12c60572
Tonyavall <tonyavallescas@gmail.com>
2022-08-01 13:53:49 -0700

    Added viewlogic for Page component

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -1,11 +1,29 @@
 import React from 'react'
 import PageContent from '../PageContent'
-
+import Home from '../Home'
+import Works from '../Works'
+import Collaborations from '../Collaborations'
+import About from '../About'
 
 export default function Page({currentPage}) {
-  switch(currentPage) {
-    case 'Home':
-    default:
-      return <Home/>
+  const changeView = page => {
+    switch(page) {
+        case 'Home':
+            return <Home/>
+        case 'Works':
+            return <Works/>
+        case 'Collaborations':
+            return <Collaborations/>
+        case 'About':
+            return <About/>
+        default:
+            return <Home/>
+        }
   }
+
+  return (
+    <section>
+        {changeView(currentPage)}
+    </section>
+  )
 }
