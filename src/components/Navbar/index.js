e700935d132bc49dc7bc0b6718d1a2e92d0bc558
Tonyavall <tonyavallescas@gmail.com>
2022-08-01 13:21:01 -0700

    Added base components for portfolio

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,24 @@
+function Navbar(props) {
+  const {
+    pages,
+    setCurrentPage,
+  } = props
+
+  return (
+    <nav className="flex justify-end">
+      {
+        pages.map(page=> (
+          <button 
+            onClick={setCurrentPage(page)} 
+            className="mx-2"
+            key={page}
+            >
+            {page}
+          </button>
+        ))
+      }
+    </nav>
+  )
+}
+
+export default Navbar
